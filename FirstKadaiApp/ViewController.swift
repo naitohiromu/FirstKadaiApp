d33fe7b3c53ba20bb65da65d6fbf78a842425373
naitohiromu <naito.hiromu01@genetec.co.jp>
2023-05-25 08:49:53 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by naito.hiromu on 2023/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
